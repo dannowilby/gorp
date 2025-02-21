f1e21b1514bf573ffaf5819391e3d0f095e31965
dannowilby <dywilby@gmail.com>
2025-02-21 13:15:25 -0800

    add basic go project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dannowilby/gorp
+
+go 1.23.5
---
+++ gorp.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func run() error {
+	return nil
+}
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Println(err)
+	}
+}
