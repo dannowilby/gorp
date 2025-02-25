97069ee007d57c3568ef287a437f6e2d12912223
dannowilby <dywilby@gmail.com>
2025-02-24 16:32:15 -0800

    create state machine for replica role

--- gorp.go
+++ gorp.go
@@ -2,9 +2,29 @@ package main
 
 import (
 	"fmt"
+	"reflect"
+
+	gorp "github.com/dannowilby/gorp/lib"
 )
 
 func run() error {
+
+	replica := gorp.Broker{
+		State: gorp.State{},
+		Role:  gorp.Candidate{},
+	}
+
+	for {
+		next_role := replica.Run()
+		fmt.Println("Next role:", reflect.TypeOf(next_role))
+
+		switch next_role.(type) {
+		case gorp.Exiting:
+			return nil
+		default:
+			replica.Role = next_role
+		}
+	}
 
 	return nil
 }
@@ -12,5 +32,7 @@ func run() error {
 func main() {
 	if err := run(); err != nil {
 		fmt.Println(err)
+	} else {
+		fmt.Println("Shut down gracefully.")
 	}
 }
--- lib/broker.go
+++ lib/broker.go
@@ -1,29 +1,64 @@
 package gorp
 
+import "fmt"
+
 type log_entry struct {
 	term    int
 	message string
 }
 
-func follower() {
-	// listen for append message rpcs to this replica
+type Role interface {
+	// sets up the RPC listener/other functionality
+	// returns the next state of the replica
+	Execute(State) Role
 }
 
-type Broker struct {
+type State struct {
 	// the set of servers participating in consensus
 	config []string
 
 	// persistent state
 	log         []log_entry
-	commit_term int
+	Commit_term int
 	voted_for   string
 
 	// volatile state
 	commit_index int
 	last_applied int
+}
 
-	// the functionality to run if leader, candidate, or follower
-	role_func func()
+type Broker struct {
+	State State
+	Role  Role
+}
+
+func (broker Broker) Run() Role {
+	return broker.Role.Execute(broker.State)
+}
+
+type Follower struct{}
+
+func (Follower) Execute(state State) Role {
+	return Follower{}
+}
+
+type Candidate struct{}
+
+func (Candidate) Execute(state State) Role {
+	fmt.Println("Running as candidate replica.")
+	return Exiting{}
+}
+
+type Leader struct{}
+
+func (Leader) Execute(state State) Role {
+	return Leader{}
+}
+
+type Exiting struct{}
+
+func (Exiting) Execute(state State) Role {
+	return Exiting{}
 }
 
 // a config change entails the following process
